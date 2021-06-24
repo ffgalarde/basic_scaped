e406d32566e42d9c5f912c704b420fb071d30278
ffgalarde <ffgalarde@outlook.com>
2021-06-23 19:38:37 -0500

    Inicializacion de proyecto basico

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Tutorial seguido
+        //https://www.youtube.com/watch?v=4I4qdmC6kFc
+        
+        IWebDriver driver = new FirefoxDriver(@"C:\Users\ffgal\OneDrive\Documentos\C#\scraped\bin\Debug\net5.0\");
+        driver.Url = "http://localhost/";
+
+        // IWebElement textbox = driver.FindElement(By.Name("q"));
+        // textbox.SendKeys("GalRod Arte en Resina");
+        
+        //textbox.Submit();
+        driver.Quit();
+    }
+}
